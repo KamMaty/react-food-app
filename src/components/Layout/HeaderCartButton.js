fb98df90d83124955b33edbe1d0b7efc829d7cb6
Kamil Matyasik <matyasik.k@wp.pl>
2022-12-18 23:35:16 +0100

    Added functionality for showing and closing cart (at least with dummy elements at the moment)

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-import CartIcon from "../Cart/Carticon";
+import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   return (
-    <button className={styles.button}>
+    <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
